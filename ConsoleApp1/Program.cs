dcea965e14be6313977959daf053238aa85d425d
blisss3three <maxym.brenyk@gmail.com>
2024-06-08 16:11:43 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void changeArr(int[] arr) 
+        {
+            arr = new int[] { 10, 20, 30 };
+        }
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3 };
+            changeArr(arr);
+            Console.WriteLine(arr[0]);
+
+        }
+    }
+}
